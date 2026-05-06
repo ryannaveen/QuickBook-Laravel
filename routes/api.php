49b69f969aa133294395e94edc00cabe99157b55
ryannaveen <ryangamalathge6@gmail.com>
2026-05-06 07:16:48 +0530

    Add Sanctum API routes structure

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,31 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AppointmentController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Public routes (no authentication needed)
+Route::post('/tokens/create', [AuthController::class, 'createToken']);
+Route::get('/services', [ServiceController::class, 'index']);
+Route::get('/services/{service}', [ServiceController::class, 'show']);
+
+// Protected routes (wrapped in auth:sanctum middleware)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::delete('/tokens/revoke', [AuthController::class, 'revokeToken']);
+    Route::post('/services', [ServiceController::class, 'store']);
+    Route::get('/appointments', [AppointmentController::class, 'index']);
+    Route::post('/appointments', [AppointmentController::class, 'store']);
+    Route::patch('/appointments/{appointment}/cancel', [AppointmentController::class, 'cancel']);
+});
